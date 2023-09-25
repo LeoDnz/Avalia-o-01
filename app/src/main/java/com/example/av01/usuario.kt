da29550dbcc6efbc9ceaa1fb8ba778fb2299eabb
LeoDnz <leonardo.oliveira6@alunos.unis.edu.br>
2023-09-25 14:42:53 -0300

    Projeto

---
+++ app/src/main/java/com/example/av01/usuario.kt
@@ -0,0 +1,11 @@
+package com.example.av01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nome_usuario)
+    }
+}
